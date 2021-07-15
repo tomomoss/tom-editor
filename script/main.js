4ad00dbde911e3e76c13a9609d433c4b68557379
tomomoss <ishida.tmys@gmail.com>
2021-07-15 20:23:54 +0900

    ファイル構成を整理しました。

--- script/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import {
-  TOMEditor
-} from "./tom-editor.mjs";
-
-const tomEditorContainer = document.querySelector(".tom-editor-container");
-const tomEditor = new TOMEditor(tomEditorContainer);
