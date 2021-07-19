918f25d1be067d168e948d695f4fa84b549d8e29
tomomoss <ishida.tmys@gmail.com>
2021-07-19 20:28:14 +0900

    ファイルを整理しました。

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@
 
 import {
   TOMEditor
-} from "./tom-editor.mjs";
+} from "../src/tom-editor.mjs";
 
 const tomEditorContainer = document.querySelector(".tom-editor-container");
 const tomEditor = new TOMEditor(tomEditorContainer);
