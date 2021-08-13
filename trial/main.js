4f8a5dd7eb68daa14576d0b4f634532d3fe81b80
tomomoss <ishida.tmys@gmail.com>
2021-08-13 13:45:46 +0900

    開発がしやすくなるようにTOMEditorオブジェクトをグローバル化しておきます。

--- trial/main.js
+++ trial/main.js
@@ -5,4 +5,4 @@ import {
 } from "../src/tom-editor.mjs";
 
 const tomEditorContainer = document.querySelector(".tom-editor-container");
-const tomEditor = new TOMEditor(tomEditorContainer);
+window.tomEditor = new TOMEditor(tomEditorContainer);
