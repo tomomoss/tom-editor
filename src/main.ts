8c0492ff4fa68c3cc71b686aeb1caefc382a93fc
tomomoss <ishida.tmys@gmail.com>
2021-08-31 06:47:52 +0900

    TypeScriptで書きなおす準備が整いました。

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import "./common.scss";
+
+/**
+ * ライブラリ「TOM Editor」のエントリポイントです。
+ */
+const Main = class extends EventTarget {
+  constructor() {
+    super();
+  }
+};
+
+export {
+  Main as TOMEditor
+}
