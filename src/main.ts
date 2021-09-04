c815ecf5529ca12c8f1f4af638123c9d3e6b4d95
tomomoss <ishida.tmys@gmail.com>
2021-09-04 14:41:27 +0900

    バージョンを4.4.3に上げました。

--- src/main.ts
+++ src/main.ts
@@ -96,7 +96,7 @@ const Main = class extends EventTarget implements Main {
    * @returns {string} ライブラリのバージョンです。
    */
   static get version(): string {
-    return "4.4.2";
+    return "4.4.3";
   }
 
   /**
